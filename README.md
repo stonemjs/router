c2bc835d3361f4ed7b48a0a122d1493d4148f6ad
Mr Stone <pierre.evens16@gmail.com>
2023-08-25 21:20:00 -0400

    Implement RequestContext

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Router
 Vanilla Javascript Backend and frontend Router with proposal decorator
+
+
+# Todo
+## Request
+The current request object
+
+## Route
+The resolved route
+
+## RouteCollection
+The predifined list routes
+
+## Controller Dispatcher
+To dispatch the current route to controller and check middleware
+
+## Event
+RouteMatched
+
+## Loaders
+To load routes from app
+Type of loaders
+1. Modules loader
+2. Decorator loader
+3. Directory loader
+4. File loader
+5. GlobFile loader
+6. Object loader
+7. Container loader
+
+## Matcher
+To match request to route
+Type of matcher
+1. UrlMatcher
+2. RequestMatcher
+3. SchemeMatcher
+4. MethdMatcher
+5. HostMatcher
+
+## Generator
+To generate url base on a given value
+1. UrlGenerator
+
+## Exception
+1. RuntimeException
+2. RouteNotFoundException
+3. ResourceNotFoundException
+4. NoConfigurationException
+5. MissingMandatoryParametersException
+6. MethodNotAllowedException
+7. InvalidParameterException
+8. InvalidArgumentException
+
+## Decorator
+1. Route
+2. Get
+3. Post
+4. Put
+5. Patch
+6. Delete
+7. Options
+8. Controller
---
+++ src/RequestContext.mjs
@@ -0,0 +1,100 @@
+/**
+ * Holds information about the current request.
+ *
+ * @author Mr. Stone <pierre.evens16@gmail.com>
+ */
+export class RequestContext {
+  constructor({
+    ip,
+    host,
+    body,
+    path,
+    query,
+    route,
+    params,
+    method,
+    locale,
+    headers,
+    baseUrl,
+    hostname,
+    protocol,
+    httpPort,
+    httpsPort,
+    queryString
+  }) {
+    this.ip = ip
+    this.host = host
+    this.body = body
+    this.path = path
+    this.query = query
+    this.route = route
+    this.params = params
+    this.method = method
+    this.locale = locale
+    this.pathInfo = path
+    this.headers = headers
+    this.baseUrl = baseUrl
+    this.hostname = hostname
+    this.protocol = protocol
+    this.httpPort = httpPort
+    this.httpsPort = httpsPort
+    this.queryString = queryString
+  }
+
+  /**
+   * Creates the RequestContext information based on a HttpFoundation Request.
+   *
+   * @param {string}  uri
+   * @param {string = 'localhost'}  hostname
+   * @param {string = 'http'}  protocol
+   * @param {number = 80}  httpPort
+   * @param {number = 443}  httpsPort
+   * 
+   * @return ${this}
+   */
+  static fromUri (uri, hostname = 'localhost', protocol = 'http', httpPort = 80, httpsPort = 443) {
+    const url = new URL(uri)
+
+    return new this({
+      method: 'GET',
+      host: url.host,
+      path: url.pathname,
+      baseUrl: url.origin,
+      queryString: url.search,
+      hostname: url.hostname ?? hostname,
+      query: this.#parseQuery(url.searchParams),
+      protocol: url.protocol.replace(':', '') ?? protocol,
+      httpPort: (url.protocol === 'http' && url.port) ?? httpPort,
+      httpsPort: (url.protocol === 'https' && url.port) ?? httpsPort,
+    })
+  }
+
+  /**
+   * Creates the RequestContext information based on a HttpFoundation Request.
+   *
+   * @param {Request} request
+   * 
+   * @return {this}
+   */
+  static fromRequest(request) {
+    return new this({ ...request })
+  }
+
+  static #parseQuery (searchParams) {
+    return Array
+      .from(searchParams)
+      .reduce((prev, [key, value]) => {
+        prev[key] = prev[key] ? (Array.isArray(prev[key]) ? [...prev[key], value] : [prev[key], value]) : value
+        return prev
+      }, {})
+  }
+  
+  /**
+   * Is Secure
+   *
+   * @return {boolean}
+   */
+  get isSecure () {
+    return 'https' === this.protocol
+  }
+}
